c7148b93737bfd3a238aaefeae50d4f2f20a987e
Trishane <trishaneww@gmail.com>
2024-01-14 14:11:36 -0500

    create file structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Homepage from './components/homepage/Homepage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path='/' element={<Homepage />} />
+        </Routes>
+      </div>
+    
+    </BrowserRouter>
+  );
+}
+
+export default App;
