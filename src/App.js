a06de560e36f936b62638439a3c545cfc160b156
Trishane <trishaneww@gmail.com>
2024-04-18 03:32:58 -0400

    homepage sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Homepage from './components/homepage/Homepage';
+import Login from './components/login/Login';
+import Signup from './components/signup/Signup';
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <div className="App">
         <Routes>
           <Route path='/' element={<Homepage />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
         </Routes>
       </div>
     
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,37 +1,69 @@
-import React from 'react';
-import logo from '../../assets/logo.png';
-import { Link, useNavigate } from 'react-router-dom';
-import { useState } from 'react';
-import axios from 'axios';
-
+import React from "react";
+import '../../styles/global.scss'
+import logo from "../../assets/logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setUser((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  console.log(user);
+
   return (
     <div>
-        <img src={logo} alt="kiosk logo" />
+      <div className="login-container">
+        <div className="curved">
+          <svg
+            className="svg"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1440 320"
+          >
+            <path
+              fill="#f21d9d"
+              fill-opacity="1"
+              d="M0,128L48,106.7C96,85,192,43,288,64C384,85,480,171,576,224C672,277,768,299,864,309.3C960,320,1056,320,1152,282.7C1248,245,1344,171,1392,133.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+            ></path>
+          </svg>
+          <img className="logo" src={logo} alt="kiosk logo" />
+        </div>
 
-        <form>
-            <input 
-                name="email"
-                type="text"
-                placeholder="email"
-                onChange={handleChange}
+        <form className="login-page">
+          <div className="login-page__fields">
+            <input
+              name="email"
+              type="text"
+              placeholder="email"
+              onChange={handleChange}
             />
+          </div>
 
-            <input 
-                name="password"
-                type="text"
-                placeholder="password"
-                onChange={handleChange}
+          <div className="login-page__fields">
+            <input
+              name="password"
+              type="text"
+              placeholder="password"
+              onChange={handleChange}
             />
+          </div>
 
-            <button>Login</button>
-            <p>Dont have an account? <Link to='/signup'>Sign up</Link></p> 
+          <button className="login-btn">Login</button>
+          {/* <p>
+            Dont have an account? <Link to="/signup">Sign up</Link>
+          </p> */}
         </form>
-
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
