de831c1f765f02457eb4e968bb6a02bf9bf61482
Trishane <trishaneww@gmail.com>
2024-04-25 19:16:24 -0400

    update margin in menu display

--- src/components/viewmenu/Viewmenu.jsx
+++ src/components/viewmenu/Viewmenu.jsx
@@ -5,7 +5,7 @@ import { Link, useNavigate } from "react-router-dom";
 import Item from "../item/Item";
 import CartItem from "../cartItem/CartItem";
 import { IoLocationSharp } from "react-icons/io5";
-import { FiLogOut } from "react-icons/fi";;
+import { FiLogOut } from "react-icons/fi";
 
 const Viewmenu = () => {
   //   const [menuItems, setMenuItems] = useState([]);
@@ -59,15 +59,15 @@ const Viewmenu = () => {
     },
     {
       id: 4,
-      name: "Burrito Combos",
+      name: "Roll Combos",
     },
     {
       id: 4,
-      name: "Signature Burritos",
+      name: "Signature Rolls",
     },
     {
       id: 4,
-      name: "Custom Burrito",
+      name: "Custom Roll",
     },
     {
       id: 5,
@@ -83,66 +83,141 @@ const Viewmenu = () => {
     },
   ];
 
-  const menuItems = [
+  const products = [
     {
       id: 1,
-      name: "Meat Lovers",
-      price: 15.25,
-      calories: 1200,
+      orderid: "AJ5S6",
+      name: "Oaxaca Bowl",
+      calories: 660,
+      price: 15.49,
+      cost: 10,
+      status: true,
     },
     {
       id: 2,
-      name: "Cheese Lovers",
-      price: 11.25,
-      calories: 1200,
+      orderid: "AK971",
+      name: "Teriyaki Twiist Bowl",
+      calories: 590,
+      price: 14.29,
+      cost: 10,
+      status: true,
     },
     {
       id: 3,
-      name: "Pepporoni Lovers",
-      price: 13.25,
-      calories: 1200,
+      orderid: "B9SS6",
+      name: "Mezze Delight Bowl",
+      calories: 630,
+      price: 13.79,
+      cost: 10,
+      status: true,
     },
     {
       id: 4,
-      name: "Canadian Lovers",
-      price: 15.25,
-      calories: 1200,
+      orderid: "Z9KP1",
+      name: "Tikka Bowl",
+      calories: 590,
+      price: 14.79,
+      cost: 10,
+      status: false,
     },
     {
       id: 5,
-      name: "Hawaiian Lovers",
-      price: 15.25,
-      calories: 1200,
+      orderid: "SP766",
+      name: "Chipolte Cheddar Bowl",
+      calories: 730,
+      price: 14.29,
+      cost: 10,
+      status: true,
     },
     {
-      id: 5,
-      name: "Hawaiian Lovers",
-      price: 15.25,
-      calories: 1200,
+      id: 6,
+      orderid: "XW290",
+      name: "Bhudda Bowl",
+      calories: 540,
+      price: 13.79,
+      cost: 10,
+      status: false,
     },
     {
-      id: 5,
-      name: "Hawaiian Lovers",
-      price: 15.25,
-      calories: 1200,
+      id: 7,
+      orderid: "TF2V3",
+      name: "Cobb Salad",
+      calories: 430,
+      price: 11.99,
+      cost: 7,
+      status: true,
     },
     {
-      id: 5,
-      name: "Hawaiian Lovers",
-      price: 15.25,
-      calories: 1200,
+      id: 8,
+      orderid: "V8NV3",
+      name: "Fiesta Salad",
+      calories: 390,
+      price: 11.49,
+      cost: 7,
+      status: true,
     },
     {
-      id: 5,
-      name: "Hawaiian Lovers",
-      price: 15.25,
-      calories: 1200,
+      id: 9,
+      orderid: "TF2V3",
+      name: "Greek Salad",
+      calories: 580,
+      price: 11.49,
+      cost: 8.75,
+      status: true,
     },
     {
-      id: 5,
-      name: "Hawaiian Lovers",
-      price: 15.25,
-      calories: 1200,
+      id: 10,
+      orderid: "TF2V3",
+      name: "Kale Ceasar Salad",
+      calories: 670,
+      price: 11.49,
+      cost: 7,
+      status: true,
+    },
+    {
+      id: 11,
+      orderid: "L9PM5",
+      name: "Berri Crunchii Salad",
+      calories: 630,
+      price: 11.99,
+      cost: 7,
+      status: false,
+    },
+    {
+      id: 12,
+      orderid: "K1PO5",
+      name: "Texas SmokeHouse Roll",
+      calories: 840,
+      price: 12.99,
+      cost: 8,
+      status: false,
+    },
+    {
+      id: 13,
+      orderid: "G6N90",
+      name: "Spicy Buffalo Roll",
+      calories: 580,
+      price: 10.99,
+      cost: 7,
+      status: false,
+    },
+    {
+      id: 14,
+      orderid: "D34LP",
+      name: "Sesame Crunch Roll",
+      calories: 490,
+      price: 9.29,
+      cost: 8,
+      status: false,
+    },
+    {
+      id: 15,
+      orderid: "A36XM",
+      name: "Zestii Zaatar Burrito",
+      calories: 530,
+      price: 11.99,
+      cost: 8,
+      status: false,
     },
   ];
 
@@ -171,13 +246,14 @@ const Viewmenu = () => {
           </section>
 
           <section className="menu-display__items">
-            {menuItems.map((item) => (
-              <Link className="link" to={`/menu/${item.id}`}>
+            {products.map((product) => (
+              <Link className="link" to={`/menu/${product.id}`}>
                 <Item
-                  key={item.id}
-                  price={item.price}
-                  name={item.name}
-                  calories={item.calories}
+                  key={product.id}
+                  id={product.id}
+                  price={product.price}
+                  name={product.name}
+                  calories={product.calories}
                 />
               </Link>
             ))}
