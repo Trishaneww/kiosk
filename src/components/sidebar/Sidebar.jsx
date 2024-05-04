5c274f665a83b942eae0beaafb74a1b85b7220a8
Trishane <trishaneww@gmail.com>
2024-05-04 01:39:52 -0400

    sidebar structure

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,57 @@
-import './Sidebar.scss';
+import "./Sidebar.scss";
+import { MdRestaurantMenu } from "react-icons/md";
+import { AiFillHome } from "react-icons/ai";
+import { TfiStatsUp } from "react-icons/tfi";
+import { HiDocumentText } from "react-icons/hi2";
+import { IoIosSettings } from "react-icons/io";
+import { GoStarFill } from "react-icons/go";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div>
+      <div className="sidebar">
+        <div className="sidebar-content">
+          <div className="sidebar-burgerContainer">
+            <div className="sidebar-burgerContainer-trigger"></div>
+            <div className="sidebar-burgerContainer-menugit"></div>
+          </div>
+          <ul>
+            <li>
+              <AiFillHome />
+              <Link to={`/`}>HOME</Link>
+            </li>
 
-export default Sidebar
+            <li>
+              <MdRestaurantMenu />
+              <Link to={`/menu`}>MENU</Link>
+            </li>
+
+            <li>
+              <HiDocumentText />
+              <Link to={`/orders`}>ORDERS</Link>
+            </li>
+
+            <li>
+              <TfiStatsUp />
+              <Link to={`/dashboard`}>Performance</Link>
+            </li>
+
+            <li>
+              <GoStarFill />
+              <Link to={`/dashboard`}>Feedback</Link>
+            </li>
+
+            <li>
+              <IoIosSettings />
+              <Link to={`/settings`}>Settings</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
