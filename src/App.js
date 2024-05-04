f878634a75678b2e84cb980715d3b9e41d80e1ff
Trishane <trishaneww@gmail.com>
2024-05-04 00:59:23 -0400

    responsive sidebar

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import Viewmenu from './components/viewmenu/Viewmenu';
 import Orders from './components/orders/Orders';
 import Dashboard from './components/dashboard/Dashboard'
 import ManageProducts from './components/manageProducts/ManageProducts';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        
         <Routes>
-          <Route path='/' element={<Homepage />} />
+          <Route path='/' element={<Sidebar  />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
           <Route path='/menu/:id' element={<Viewmenu />} />
